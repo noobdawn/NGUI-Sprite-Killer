de7fbc3e19f37d3557ed5dd7973f6c7f7d501337
noobdawn <694109410@qq.com>
2018-06-23 14:54:01 +0800

    重写界面

--- NGUIUnusedSpriteFound.cs
+++ NGUIUnusedSpriteFound.cs
@@ -36,74 +36,67 @@ public class NGUIUnusedSpriteFound : EditorWindow
         NGUIUnusedSpriteFound window = GetWindow<NGUIUnusedSpriteFound>();
         window.Show();
         window.Init();
+        window.CollectAllSpriteInfo();
+        window.FilterAllSpriteInfo();
         window.name = "检测无效图集";
-
-        myStyle = new GUIStyle();
-        myStyle.fontSize = 15;
-        myStyle.normal.textColor = Color.white;
     }
 
     private void OnGUI()
     {
         GUILayout.BeginVertical();
-        if (GUILayout.Button("收集所有图集信息",GUILayout.Width(380)))
-        {
-            CollectAllSpriteInfo();
-        }
-        if (GUILayout.Button("开始比对UIPrefab", GUILayout.Width(380)))
-        {
-            FilterAllSpriteInfo();
-        }
-        if (GUILayout.Button("删除选中Sprite",   GUILayout.Width(380)))
+        GUILayout.TextArea("比对结果仅供参考！\n代码中赋值的图标和表格里配置的图标不计入比对结果！\n请只删除那些确定不会再使用的图片，宁放过勿错过！\n对图集做出修改后请Save Project后再提交SVN。");
+        GUILayout.EndVertical();
+        GUILayout.BeginVertical();
+        if (GUILayout.Button("删除选中Sprite",GUILayout.Height(30)))
         {
             DeleteAllSprite();
         }
         DrawSpriteInfo();
         GUILayout.EndVertical();
-        GUILayout.BeginVertical();
-        GUILayout.TextArea("比对结果仅供参考！\n代码中赋值的图标和表格里配置的图标不计入比对结果！\n请只删除那些确定不会再使用的图片，宁放过勿错过！\n对图集做出修改后请Save Project后再提交SVN。");
-        GUILayout.EndVertical();
     }
 
     public Vector2 mListScrollPos;
-    public bool mIsListExpand;
+    //public bool mIsListExpand;
     public bool mIsDicExpand;
     private void DrawSpriteInfo()
     {
         if (spriteInfoList == null) return;
-        mListScrollPos = EditorGUILayout.BeginScrollView(mListScrollPos, GUILayout.Width(380));
-        mIsListExpand = EditorGUILayout.Foldout(mIsListExpand, "图片列表");
-        if (mIsListExpand)
-        {
-            foreach (var info in spriteInfoList)
-            {
-                EditorGUILayout.BeginHorizontal(GUILayout.Width(380));
-                GUILayout.Space(30);
-                GUILayout.Label(info.Atlas.name + " - " + info.Data.name);
-                //info.Selected = EditorGUILayout.ToggleLeft(info.SpriteName, info.Selected, GUILayout.Width(240));
-                EditorGUILayout.EndHorizontal();
-            }
-        }
+        mListScrollPos = EditorGUILayout.BeginScrollView(mListScrollPos);
+        //mIsListExpand = EditorGUILayout.Foldout(mIsListExpand, "图片列表");
+        //if (mIsListExpand)
+        //{
+        //    foreach (var info in spriteInfoList)
+        //    {
+        //        EditorGUILayout.BeginHorizontal(GUILayout.Width(380));
+        //        GUILayout.Space(30);
+        //        GUILayout.Label(info.Atlas.name + " - " + info.Data.name);
+        //        //info.Selected = EditorGUILayout.ToggleLeft(info.SpriteName, info.Selected, GUILayout.Width(240));
+        //        EditorGUILayout.EndHorizontal();
+        //    }
+        //}
         mIsDicExpand = EditorGUILayout.Foldout(mIsDicExpand, "比对结果");
         if (mIsDicExpand)
         {
             int i = 0;
+            int size = 80;
+            int padded = 90;
+            int columns = Mathf.FloorToInt(Screen.width / padded);
+            if (columns < 1) columns = 1;
+            GUILayout.Space(100);
             foreach (var info in spriteInfoDic)
             {
-                GUILayout.Space(65);
-                EditorGUILayout.BeginHorizontal(GUILayout.Width(380));
-                GUILayout.Space(90);
+                #region 图标绘制
+                int y = i / columns;
+                int x = i - columns * y;
                 Texture2D tex = info.Value.Atlas.texture as Texture2D;
-                Rect rect = new Rect(10f, i * 85 + 40, 80f, 80f);
+                Rect rect = new Rect(10f + x * (padded), 20 + y * (padded + 20), size, size);
                 Rect uv  = new Rect(info.Value.Data.x, info.Value.Data.y, info.Value.Data.width, info.Value.Data.height);
                 uv = NGUIMath.ConvertToTexCoords(uv, tex.width, tex.height);
                 float scaleX = rect.width / uv.width;
                 float scaleY = rect.height / uv.height;
-
-                // Stretch the sprite so that it will appear proper
+                //把图标拉伸到正常大小
                 float aspect = (scaleY / scaleX) / ((float)tex.height / tex.width);
                 Rect clipRect = rect;
-
                 if (aspect != 1f)
                 {
                     if (aspect < 1f)
@@ -121,11 +114,26 @@ public class NGUIUnusedSpriteFound : EditorWindow
                         clipRect.yMax -= padding;
                     }
                 }
-
+                //画出外框
+                if (info.Value.Selected)
+                    NGUIEditorTools.DrawOutline(new Rect(rect.x - 1, rect.y - 1, rect.width + 2, rect.height + 2), Color.green);
+                else
+                    NGUIEditorTools.DrawOutline(new Rect(rect.x - 1, rect.y - 1, rect.width + 2, rect.height + 2), Color.grey);
+                //画出按钮
+                if (GUI.Button(rect, ""))
+                {
+                    info.Value.Selected = !info.Value.Selected;
+                }
                 GUI.DrawTextureWithTexCoords(clipRect, tex, uv);
-                NGUIEditorTools.DrawOutline(rect, new Color(0.4f, 1f, 0f, 1f));
-                info.Value.Selected = EditorGUILayout.ToggleLeft(info.Value.Data.name, info.Value.Selected, myStyle, GUILayout.Width(240));
-                EditorGUILayout.EndHorizontal();
+                #endregion
+                #region 文字绘制和空白留出
+                GUI.Label(new Rect(rect.x, rect.y + size, rect.width, rect.height), info.Value.Data.name);
+                GUI.Label(new Rect(rect.x, rect.y + size + 10, rect.width, rect.height), info.Value.Atlas.name);
+                if (x == 0)
+                {
+                    GUILayout.Space(100);
+                }
+                #endregion
                 i++;
             }
         }
@@ -145,7 +153,7 @@ public class NGUIUnusedSpriteFound : EditorWindow
     {
         spriteInfoList.Clear();
         var atlasFiles = Directory.GetFiles(atlasPath, "*.prefab", SearchOption.AllDirectories);
-        EditorUtility.DisplayDialog("", "检测到" + atlasFiles.Length + "个图集", "OK");
+        Debug.Log("检测到" + atlasFiles.Length + "个图集");
         foreach (string atlasFile in atlasFiles)
         {
             UIAtlas atlas = AssetDatabase.LoadAssetAtPath<GameObject>(atlasFile.Substring(atlasFile.IndexOf("Assets"))).GetComponent<UIAtlas>();
@@ -159,12 +167,15 @@ public class NGUIUnusedSpriteFound : EditorWindow
                 spriteInfoList.Add(new SpriteInfo(v, atlas));
             }
         }
-        EditorUtility.DisplayDialog("", "总计：" + spriteInfoList.Count + "个Sprite", "OK");
+        Debug.Log("总计：" + spriteInfoList.Count + "个Sprite");
     }
 
     private void FilterAllSpriteInfo()
     {
         spriteInfoDic.Clear();
+        int pro = 0;
+        int count = spriteInfoList.Count;
+        EditorUtility.DisplayProgressBar("", "", (float)pro / count);
         foreach (var v in spriteInfoList)
         {
             if (spriteInfoDic.ContainsKey(v.Data.name))
@@ -175,11 +186,15 @@ public class NGUIUnusedSpriteFound : EditorWindow
             spriteInfoDic.Add(v.Data.name, v);
         }
         var prefabFiles = Directory.GetFiles(prefabPath, "*.prefab", SearchOption.AllDirectories);
-        EditorUtility.DisplayDialog("", "有" + prefabFiles.Length + "个Prefab待比对", "OK");
+        Debug.Log("有" + prefabFiles.Length + "个Prefab待比对");
         foreach (string prefabFile in prefabFiles)
         {
+            pro++;
+            EditorUtility.DisplayProgressBar("检测", prefabFile, (float)pro / count);
             FilterObjectRef(prefabFile.Substring(prefabFile.IndexOf("Assets")));
         }
+        EditorUtility.ClearProgressBar();
+
     }
 
     void FilterObjectRef(string assetPath)
